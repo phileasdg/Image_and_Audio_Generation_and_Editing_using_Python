573a2fcfe9e0b016b9220943e36e19aebfde7dda
Phileas Dazeley-Gaist <60669698+phileasdg@users.noreply.github.com>
2021-09-30 01:48:20 -0400

    Video reading test

--- media_tools/video_tools.py
+++ media_tools/video_tools.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import os.path
 
 """
 read video data from a file and returns a video data array
@@ -14,8 +15,10 @@ def read_video(path):
         ret, frame = video.read()  # read one frame from the video capture object
         if ret:
             frames.append(frame)
+    fps = video.get(cv2.CAP_PROP_FPS)
+    video.release()
     video = np.stack(frames, axis=0)
-    return video
+    return video, fps
 
 
 """
@@ -24,17 +27,35 @@ read video data from the device camera and returns a video data array
 
 
 def read_video_from_camera():
-    pass
+    frames = []
+    video = cv2.VideoCapture(0)
+    ret = True
+    while ret:
+        ret, frame = video.read()  # read one frame from the video capture object
+        if ret:
+            frames.append(frame)
+    fps = video.get(cv2.CAP_PROP_FPS)
+    video.release()
+    video = np.stack(frames, axis=0)
+    return video, fps
 
 
-def save_video(video, path="saved_video.avi"):
+"""
+save a video array as a video file
+"""
+
+
+def save_video(video, fps=30, path="saved_video.avi"):
+    height, width = video.shape[1:3]
+
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    length, width = video.shape[1:3]
-    video_out = cv2.VideoWriter(path, fourcc, format(video.get(cv2.CAP_PROP_FPS)))
+    video_out = cv2.VideoWriter(path, fourcc, fps, (width, height))
+
     for frame in video:
         video_out.write(frame)
     video_out.release()
-    cv2.destroyAllWindows()
+    if os.path.isfile(path):
+        print("Video saved at:", path)
 
 
 def show_video():
