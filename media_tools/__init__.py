1cb0d78456048e709db553f2a3f6429ec2b829fc
Phileas Dazeley-Gaist <60669698+phileasdg@users.noreply.github.com>
2021-10-06 00:27:44 -0400

    Added tools to the media_tools package

--- media_tools/__init__.py
+++ media_tools/__init__.py
@@ -1,1 +1,6 @@
-__all__ = ["audio_tools", "data_tools", "image_tools", "video_tools"]
+__all__ = ["audio_tools",
+           "data_tools",
+           "image_tools",
+           "video_tools",
+           "destructive_effects",
+           "nondestructive_effects"]
