90f0e10ba51a1e54818488d8e05560eb80f598d3
phileasdg <pdazeley-gaist23@coa.edu>
2021-11-02 18:08:49 -0400

    deleted references to deleted files

--- media_tools/__init__.py
+++ media_tools/__init__.py
@@ -1,7 +1,4 @@
 __all__ = ["audio_tools",
            "data_tools",
            "image_tools",
-           "video_tools",
-           "destructive_effects",
-           "nondestructive_effects",
-           "effects"]
+           "video_tools"]
