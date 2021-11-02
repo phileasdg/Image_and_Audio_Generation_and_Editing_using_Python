281a05217e7b6fc912484a63c26557b332258519
phileasdg <pdazeley-gaist23@coa.edu>
2021-11-01 21:27:00 -0400

    example on 1d array to 2d array conversion steps

---
+++ misc/completing_arrays.py
@@ -0,0 +1,24 @@
+import numpy as np
+from media_tools import *
+
+sample_media = "C:/Users/pdazeley-gaist23/PycharmProjects/Image_and_Audio_Generation_and_Editing_using_Python/sample_media/"
+
+image = np.array([[1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+                  [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+                  [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+                  [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+                  [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+                  [0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
+                  ])
+
+audio = np.random.randint(0, 255, 231168)  # len = 231168
+audio = audio_tools.read_audio(sample_media+"stereo_sample.wav", mono=True, return_sr=False)
+
+image_greyscale = image_tools.array_1d_to_grayscale(audio)
+image_tools.show_image(image_greyscale)
+
+image_colour = image_tools.array_1d_to_rgb(audio)
+image_tools.show_image(image_colour)
+image_tools.show_image(data_tools.invert_array(image_colour))
+
+# gray cox work submit: this paycheck for the week ending on sunday the 31st of October.
