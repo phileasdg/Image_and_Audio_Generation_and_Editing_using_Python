79013318499c804022fb20d130f6ed6d05de78e2
phileasdg <pdazeley-gaist23@coa.edu>
2021-11-04 17:48:58 -0400

    added functions: - invert_image() - rotate_image() - rotate an image array around a point (default is the centre of the image)
    TODO:
    check if rotate_image() works for all 2d arrays, if so, its contents should be moved to data_tools.py, and rotate_image() in image_tools.py should call the function from data_tools.py with specialised settings

--- media_tools/image_tools.py
+++ media_tools/image_tools.py
@@ -2,6 +2,7 @@ import cv2
 import matplotlib.pyplot as plt
 from pathlib import Path
 from . import data_tools
+import numpy as np
 
 """
 read an image as an array
@@ -78,3 +79,48 @@ def array_1d_to_rgb(array, add_empty_pixels=0, select=-1, wide=False, return_pos
     array = data_tools.array_1d_to_2d(array, add_empty_pixels=add_empty_pixels, channels_per_pixel=3, select=select,
                                       wide=wide, return_possible_shapes=return_possible_shapes)
     return array.astype(int)
+
+
+"""
+invert an image array
+"""
+
+
+def invert_image(image):
+    return 255 - image
+
+
+"""
+rotate an image array around a point (default is the centre of the image)
+
+# cv2 border types: # cv2.BORDER_CONSTANT, cv2.BORDER_REPLICATE, cv2.BORDER_REFLECT, cv2.BORDER_WRAP, 
+cv2.BORDER_REFLECT_101, cv2.BORDER_DEFAULT 
+"""
+
+
+def rotate_image(image, angle, bound=True, point=None, border_type=cv2.BORDER_CONSTANT, border_value=(0, 0, 0)):
+    if point is None:
+        # make the point of rotation the center of the image
+        point = tuple(np.array(image.shape[1::-1]) / 2)
+
+    # get height and width of the image
+    h, w = image.shape[:2]
+
+    if bound:
+        # get the rotation matrix
+        matrix = cv2.getRotationMatrix2D(point, -angle, scale=1.0)
+        # get the sine and cosine
+        cos = np.abs(matrix[0, 0])
+        sin = np.abs(matrix[0, 1])
+        # compute the new bounding dimensions of the image
+        nH = int((h * cos) + (w * sin))
+        nW = int((h * sin) + (w * cos))
+        # adjust the rotation matrix to take into account translation
+        matrix[0, 2] += (nW / 2) - point[0]
+        matrix[1, 2] += (nH / 2) - point[1]
+        # perform the actual rotation and return the image
+        return cv2.warpAffine(image, matrix, dsize=(nW, nH), borderMode=border_type, borderValue=border_value)
+    else:
+        # rotate the image
+        return cv2.warpAffine(image, cv2.getRotationMatrix2D(point, -angle, 1.0), image.shape[1::-1],
+                              borderMode=border_type, borderValue=border_value)
